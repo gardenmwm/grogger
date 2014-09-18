fac1f4d4ea82bc76d1eee6094270b84bffd7559f
Matthew Marshall <mm17@azdeq.gov>
2014-09-18 15:00:05 -0700

    Rearranged stuff to allow easy additions of files

--- grogger.go
+++ grogger.go
@@ -5,61 +5,90 @@ import (
         "github.com/blakesmith/go-grok"
         "github.com/ActiveState/tail"
         "encoding/json"
+        "time"
         "sync"
-)
+//        "github.com/likexian/simplejson"
+        )
 
+type logentry struct {
+    logtext string
+    logtime string
+}
 
+type JSONLogEntry struct {
+    Host string
+    Timestamp string
+    Fields map[string][]string
+}
 
-//Channels
+type FullLogEntry struct {
+    hostname string
+    timestamp string
+    fields map[string][]string
+}
 
+func GetChannel() chan logentry {
+    newchan := make(chan logentry)
+    return newchan
+}
 
-func main() {
-    logtext := make(chan string)
+func MonitorLog(logfile string, pattern string){
+    logchan := GetChannel()
     var wg sync.WaitGroup
     wg.Add(2)
-    fmt.Println("Calling taillog")
-    go taillog("/tmp/test.txt",logtext, &wg)
-    fmt.Println("Calling parseLogLine")
-    go parseLogLine(logtext, &wg)
+    go taillog(logfile, logchan, &wg)
+    go parseLogLine(logchan, pattern, &wg)
     wg.Wait()
 }
 
-func taillog(file string, c chan string, wg *sync.WaitGroup){
-    fmt.Println("Enterting Taillog")
+func main() {
+    MonitorLog("/tmp/test.txt","%{WORD}")
+}
+
+func taillog(file string, c chan logentry, wg *sync.WaitGroup){
     t, err := tail.TailFile(file, tail.Config{
         Follow: true,
         ReOpen: true})
         for line := range t.Lines {
-            c <- line.Text
-            fmt.Println(line.Text)
+            logline := logentry{}
+            logline.logtext  = line.Text
+            logline.logtime = time.Now().Format(time.RFC850)
+            c <- logline
         }
     if err != nil {
-        fmt.Println("error tailing file")
+        fmt.Println("error tailing file: ", err)
     }
     wg.Done()
 }
 
-func convertToJSON(matches *grok.Match) string {
-    str, err := json.Marshal(matches.Captures())
+func convertToJSON(jsondata FullLogEntry) string {
+    l := JSONLogEntry {
+        Host: jsondata.hostname,
+        Timestamp: jsondata.timestamp,
+        Fields: jsondata.fields,
+        }
+    j,err := json.Marshal(l)
     if err != nil {
-        fmt.Println("Error encoding json: ", err)
-    }
-    return string(str)
+        fmt.Println("test")
+        }
+    return string(j)
 }
 
-func parseLogLine(c chan string, wg *sync.WaitGroup) {
-    fmt.Println("entering parseLogLine")
+func parseLogLine(c chan logentry, pattern string, wg *sync.WaitGroup) {
     g := grok.New()
     g.AddPatternsFromFile("/tmp/base")
-    pattern := "%{WORD}"
     err := g.Compile(pattern)
     if err != nil {
         fmt.Println("Error Compiling: ",err)
     }
     for {
-        line := <-c
-        jsoncapture := convertToJSON(g.Match(line))
-        fmt.Println(jsoncapture)
+        logline := <-c
+        logdata := FullLogEntry{}
+        logdata.hostname = "test"
+        logdata.timestamp = logline.logtime
+        logdata.fields= g.Match(logline.logtext).Captures()
+        jsoncapture := convertToJSON(logdata)
+        fmt.Println("parseLogLine_jsoncapture: ",jsoncapture)
     }
     wg.Done()
 }
