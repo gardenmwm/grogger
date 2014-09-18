c39cec3a804fcbbda9995d5de846b9b3a3595506
Matthew Marshall <mm17@azdeq.gov>
2014-09-18 10:50:03 -0700

    Skeleton structure created.

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ grogger
 =======
 
 Logwatcher that parses logs with grok before sending on
+
+Notes for OSX Dev:
+    If using brew modify both the grok recipe and the prce recipe, the grok recipe needs to be only a make, so the library is created, and pcre needs to be for version 7.8.
--- grogger.go
+++ grogger.go
@@ -4,30 +4,62 @@ import (
         "fmt"
         "github.com/blakesmith/go-grok"
         "github.com/ActiveState/tail"
+        "encoding/json"
+        "sync"
 )
 
 
 
 //Channels
 
+
 func main() {
+    logtext := make(chan string)
+    var wg sync.WaitGroup
+    wg.Add(2)
+    fmt.Println("Calling taillog")
+    go taillog("/tmp/test.txt",logtext, &wg)
+    fmt.Println("Calling parseLogLine")
+    go parseLogLine(logtext, &wg)
+    wg.Wait()
+}
+
+func taillog(file string, c chan string, wg *sync.WaitGroup){
+    fmt.Println("Enterting Taillog")
+    t, err := tail.TailFile(file, tail.Config{
+        Follow: true,
+        ReOpen: true})
+        for line := range t.Lines {
+            c <- line.Text
+            fmt.Println(line.Text)
+        }
+    if err != nil {
+        fmt.Println("error tailing file")
+    }
+    wg.Done()
+}
+
+func convertToJSON(matches *grok.Match) string {
+    str, err := json.Marshal(matches.Captures())
+    if err != nil {
+        fmt.Println("Error encoding json: ", err)
+    }
+    return string(str)
+}
+
+func parseLogLine(c chan string, wg *sync.WaitGroup) {
+    fmt.Println("entering parseLogLine")
     g := grok.New()
-//    g.AddPatternsFromFile("/tmp/base")
-    g.AddPattern("WORD", ".*")
+    g.AddPatternsFromFile("/tmp/base")
     pattern := "%{WORD}"
     err := g.Compile(pattern)
     if err != nil {
-        fmt.Println("Error:",err)
+        fmt.Println("Error Compiling: ",err)
     }
-
-    t, err := tail.TailFile("/tmp/test.txt", tail.Config{
-        Follow: true,
-        ReOpen: true})
-    for line := range t.Lines {
-        fmt.Println(line.Text)
+    for {
+        line := <-c
+        jsoncapture := convertToJSON(g.Match(line))
+        fmt.Println(jsoncapture)
     }
-    if err != nil {
-    fmt.Println("all goo")}
+    wg.Done()
 }
-
-
